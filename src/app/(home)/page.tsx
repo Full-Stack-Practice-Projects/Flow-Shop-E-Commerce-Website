010e79c6f102e707832064f5f2581e54b9d92188
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-06-08 09:50:54 +0300

    feat(ui): add loading skeleton for featured products section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { Suspense } from "react";
+import FeaturedProductsLoadingSkeleton from "./components/featured-products-section/FeaturedProductsLoadingSkeleton";
 import FeaturedProductsSection from "./components/featured-products-section/FeaturedProductsSection";
 import HeroSection from "./components/hero-section/HeroSection";
 
@@ -9,7 +10,7 @@ export default async function Home() {
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
       <HeroSection />
       {/** We will add a fallback to show until its childen finish loading */}
-      <Suspense fallback={"loading ...."}>
+      <Suspense fallback={<FeaturedProductsLoadingSkeleton />}>
         <FeaturedProductsSection />
       </Suspense>
     </main>
