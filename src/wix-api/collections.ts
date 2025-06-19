59c711cfcbcc11591e845a40114bd96f25f8a6c5
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-06-19 21:18:55 +0300

    feat: add cached getCollection function to fetch product collections excluding special categories and wrap getCollectionBySlug with cache function

--- src/wix-api/collections.ts
+++ src/wix-api/collections.ts
@@ -1,7 +1,27 @@
 import { WixClient } from "@/lib/wix-client.base";
+import { collections } from "@wix/stores";
+import { cache } from "react";
 
-export async function getCollectionBySlug(wixClient: WixClient, slug: string) {
-  const { collection } = await wixClient.collections.getCollectionBySlug(slug);
+export const getCollectionBySlug = cache(
+  async (wixClient: WixClient, slug: string) => {
+    const { collection } =
+      await wixClient.collections.getCollectionBySlug(slug);
 
-  return collection || null;
-}
+    return collection || null;
+  },
+);
+
+const ALL_PRODUCTS_CATEGORY_ID = "00000000-000000-000000-000000000001";
+const FEATURED_PRODUCTS_CATEGORY_ID = "a08c43aa-7b2c-3ff5-5ef3-b264a053ff5f";
+
+export const getCollection = cache(
+  async (wixClient: WixClient): Promise<collections.Collection[]> => {
+    const collections = await wixClient.collections
+      .queryCollections()
+      .ne("_id", ALL_PRODUCTS_CATEGORY_ID)
+      .ne("_id", FEATURED_PRODUCTS_CATEGORY_ID)
+      .find();
+
+    return collections.items;
+  },
+);
