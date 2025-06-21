d38efffa4d9c6a567648cf21c31808c3489927c6
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-06-21 19:44:44 +0300

    feat(search): implement SearchFilterLayout component with collection filter checkboxes synced to URL search params

---
+++ src/app/shop/layout.tsx
@@ -0,0 +1,19 @@
+import { getWixServerClient } from "@/lib/wix-server.base";
+import { getCollections } from "@/wix-api/collections";
+import React from "react";
+import SearchFilterLayout from "./components/SearchFilterLayout";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function Layout({ children }: LayoutProps) {
+  const wixServerClient = await getWixServerClient();
+  const collections = await getCollections(wixServerClient);
+
+  return (
+    <SearchFilterLayout collections={collections}>
+      {children}
+    </SearchFilterLayout>
+  );
+}
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -25,23 +25,15 @@ export default async function Page({ searchParams }: PageProps) {
   const title = q ? `Results for ${q}` : `Products`;
 
   return (
-    <main className="flex flex-col items-center justify-center gap-10 px-5 py-10 lg:flex-row lg:items-start">
-      <div>Filtter side bar</div>
-      <div className="w-full max-w-7xl space-y-5">
-        <div className="flex justify-center lg:justify-end">Sort filtter</div>
-        <div className="space-y-10">
-          <h1 className="text-center text-3xl font-bold md:text-4xl">
-            {title}
-          </h1>
-          {/** Makes Suspense changes based on the change of page and q */}
-          <Suspense
-            fallback={<ProductResultsLoadingSkeleton />}
-            key={`${q}-${page}`}
-          >
-            <ProductResults page={parseInt(page)} q={q} />
-          </Suspense>
-        </div>
-      </div>
-    </main>
+    <div className="space-y-10">
+      <h1 className="text-center text-3xl font-bold md:text-4xl">{title}</h1>
+      {/** Makes Suspense changes based on the change of page and q */}
+      <Suspense
+        fallback={<ProductResultsLoadingSkeleton />}
+        key={`${q}-${page}`}
+      >
+        <ProductResults page={parseInt(page)} q={q} />
+      </Suspense>
+    </div>
   );
 }
