fb2e995756c4895b5913613aa091d181ed9c8f58
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-06-22 19:39:36 +0300

    feat: implement getCheckoutUrlForCurrentCart to create checkout and redirect session using Wix API

---
+++ src/wix-api/checkout.ts
@@ -0,0 +1,26 @@
+import { env } from "@/env";
+import { WixClient } from "@/lib/wix-client.base";
+import { checkout } from "@wix/ecom";
+
+export async function getCheckoutUrlForCurrentCart(wixClient: WixClient) {
+  const { checkoutId } =
+    await wixClient.currentCart.createCheckoutFromCurrentCart({
+      channelType: checkout.ChannelType.WEB,
+    });
+
+  const { redirectSession } = await wixClient.redirects.createRedirectSession({
+    ecomCheckout: { checkoutId },
+    callbacks: {
+      // Where we want to redirect if checkout is over (done).
+      // Where we get redirected to if abort (cancle) checkout.
+      postFlowUrl: window.location.href,
+      thankYouPageUrl: `${env.NEXT_PUBLIC_BASE_URL}/checkout-success`,
+    },
+  });
+
+  if (!redirectSession) {
+    throw Error("Failed to create redirect session.");
+  }
+
+  return redirectSession.fullUrl;
+}
